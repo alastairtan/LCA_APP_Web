bc8aa1ef9267ff4e0d02bc9e90f205707023d04f
Song Weiyang <e0014576@u.nus.edu>
2019-04-02 16:48:14 +0800

    included Environmental Matrixd

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -17,7 +17,11 @@ export class ResultComponent implements OnInit {
     process: any[] = [];
     processName: String[] = [];
     economicflow: String[] = [];
+    environmentalflow: String[] = [];
+
+    //display matrix 
     result: any[] = [];
+    resultEnvironmental: any[] = [];
 
     constructor(private dataService: DataService,
                 private router: Router,
@@ -25,8 +29,9 @@ export class ResultComponent implements OnInit {
 
     ngOnInit() {
         this.sign();
-        this.transformingDataIntoMatrix();
+        this.transformingDataIntoMatrix(this.economicflow, this.process, this.result);
         this.checkIsProcessNodeSource();
+        this.geratingEnvironmentalMatrix();
     }
 
     sign() {
@@ -72,6 +77,15 @@ export class ResultComponent implements OnInit {
         return null;
     }
 
+    environmentVarexist(name: String) {
+        for (let i = 0; i < this.environmentalflow.length; i++) {
+            if (this.environmentalflow[i] == name) {
+                return i;
+            }
+        }
+        return null;
+    }
+
     insertUnit(index, unit: Number, processUnit: Number[]) {
         if (processUnit.length < index) {
             let diff: any = index - processUnit.length + 1;
@@ -85,18 +99,18 @@ export class ResultComponent implements OnInit {
         }
     }
 
-    transformingDataIntoMatrix() {
-        for (let i = 0; i < this.economicflow.length; i++) {
+    transformingDataIntoMatrix(label, data, result) {
+        for (let i = 0; i < label.length; i++) {
             let row: any[] = [];
-            row.push(this.economicflow[i]);
-            for (let j = 0; j < this.processName.length; j++) {
-                if (this.process[j][i] == undefined) {
+            row.push(label[i]);
+            for (let j = 0; j < data.length; j++) {
+                if (data[j][i] == undefined) {
                     row.push(0);
                 } else {
-                    row.push(this.process[j][i]);
+                    row.push(data[j][i]);
                 }
             }
-            this.result.push(row);
+            result.push(row);
         }
     }
 
@@ -125,6 +139,45 @@ export class ResultComponent implements OnInit {
         }
         
         
+    }
+
+    geratingEnvironmentalMatrix() {
+        let matrix: any[] = [];
+        for (let i = 0; i < this.project.processNodes.length; i++) {
+            let node = this.project.processNodes[i];
+            let emissionArray = node.directEmissions;
+            let vector = [];
+            for (let j = 0; j < emissionArray.length; j++) {
+                let emission = emissionArray[j];
+                //if the name of the emission exist 
+                let index = this.environmentVarexist(emission.emissionType);
+                if (index != null) {
+                    while (vector.length - 1 < index) {
+                        vector.push(0);
+                    }
+                    vector[index] = emission.quantity;
+                } else {
+                    this.environmentalflow.push(emission.emissionType);
+                    vector.push(emission.quantity);
+                }
+            }
+            if (emissionArray.length != 0) {
+                matrix.push(vector);
+            }
+        }
+        while (matrix.length < this.processName.length) {
+            let length = matrix[0].length;
+            let vector = [];
+            for (let j = 0; j < length; j++) {
+                vector.push(0);
+            }
+            matrix.push(vector);
+        }
+        //check if length of columns is equal to process name
+        this.transformingDataIntoMatrix(this.environmentalflow, matrix, this.resultEnvironmental);
+        console.log(this.environmentalflow);
+        console.log(matrix);
+        console.log(this.resultEnvironmental);
     }
     /**Save the current project to session storage, and navigate to the previous page */
     navPrev() {
